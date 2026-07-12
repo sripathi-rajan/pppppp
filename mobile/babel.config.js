a1a8caf8f14e460d0fdb7697d37f3622219eb2c1
Dakshan karthic <dakshankarthic6@gmail.com>
2026-07-12 17:23:05 +0530

    Fix babel private properties error

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: [
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-methods'
+    ],
   };
 };
